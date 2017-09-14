222a17285cf066fb488b5d4a88a09a38d92c7c5b
Ben Holme <benjamin.holme0@gmail.com>
2017-09-14 13:56:54 +0200

    added Procfile

--- server.js
+++ server.js
@@ -22,15 +22,15 @@ var SampleApp = function() {
      */
     self.setupVariables = function() {
         //  Set the environment variables we need.
-        self.ipaddress = process.env.IP;
         self.port      = process.env.PORT || 3000;
+        //self.ipaddress = process.env.IP;
 
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        }
+        // if (typeof self.ipaddress === "undefined") {
+        //     //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+        //     //  allows us to run/test the app locally.
+        //     console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+        //     self.ipaddress = "127.0.0.1";
+        // }
     };
 
     /**
@@ -225,11 +225,10 @@ var SampleApp = function() {
      */
     self.start = function() {
         //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
+        self.app.listen(self.port, function() {
             console.log('%s: Node server started on %s:%d ...',
-            Date(Date.now() ), self.ipaddress, self.port);
+            Date(Date.now() ), self.port);
         });
-        console.log(self.ipaddress);
     };
 
 };   /*  Sample Application.  */
