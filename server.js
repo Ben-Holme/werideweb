3f7c41c501163b41925dea17d1a0832c4e7eb466
B16\Benjamin <benjamin.holme0@gmail.com>
2019-03-13 19:39:17 +0100

    new IP 83.248.218.47

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ var SampleApp = function() {
 
 
     // API FUNCTIONS
-    var ip = '83.254.65.120';
+    var ip = '83.248.218.47';
 
     self.getServerStats = function (callback) {
       var request = require('request');
