a7973a9174cde2c454ea73aadc9f47bbb81be095
BENJAMINS-SURFA\benjamin <benjamin.holme0@gmail.com>
2019-04-17 16:41:54 +0200

    blog dates

--- dev/js/getBlog/getBlog.js
+++ dev/js/getBlog/getBlog.js
@@ -16,6 +16,27 @@ define([
   GetBlog.prototype.getData = function () {
     var that = this;
     data.done(function (body) {
+
+      var monthNames = [
+        "January", "February", "March",
+        "April", "May", "June", "July",
+        "August", "September", "October",
+        "November", "December"
+      ];
+
+      for (var key in body) {
+        var jsDate = new Date(body[key].date);
+
+
+        var day = jsDate.getDate();
+        var monthIndex = jsDate.getMonth();
+        var year = jsDate.getFullYear();
+
+          var niceDate = day + ' ' + monthNames[monthIndex] + ' ' + year;
+        console.log(niceDate);
+        body[key].date = niceDate;
+      }
+
       that.render(body);
     });
   };
